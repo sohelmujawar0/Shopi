a136f23db59c43665ca8643c8dc403574b0c6456
sohelmujawar0 <sohelmujawar099@gmail.com>
2025-11-26 22:03:21 +0530

    Made responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,40 +6,33 @@ import whatsapp_icon from "../../src/assets/whatsapp.png";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-[50px]">
-      
-  
-      <div className="flex items-center gap-[20px]">
-        <img src={logo} alt="" className="h-[100px]" />
-        <p className="text-[#383838] text-[40px] font-bold">Shopifi</p>
+    <div className="flex flex-col justify-center items-center gap-8 py-10 px-4 sm:px-6 md:px-10 bg-white">
+      <div className="flex flex-col sm:flex-row items-center gap-4 sm:gap-6">
+        <img src={logo} alt="Logo" className="h-16 sm:h-20" />
+        <p className="text-[#383838] text-2xl sm:text-4xl font-bold">Shopifi</p>
       </div>
 
-      <ul className="flex list-none gap-[50px] text-[#252525] text-[20px]">
-        <li className="cursor-pointer">About</li>
-        <li className="cursor-pointer">Products</li>
-        <li className="cursor-pointer">Offices</li>
-        <li className="cursor-pointer">Company</li>
-        <li className="cursor-pointer">Contact</li>
+      <ul className="flex flex-wrap justify-center list-none gap-4 sm:gap-6 text-[#252525] text-base sm:text-lg">
+        <li className="cursor-pointer hover:text-[#ff4141] transition">About</li>
+        <li className="cursor-pointer hover:text-[#ff4141] transition">Products</li>
+        <li className="cursor-pointer hover:text-[#ff4141] transition">Offices</li>
+        <li className="cursor-pointer hover:text-[#ff4141] transition">Company</li>
+        <li className="cursor-pointer hover:text-[#ff4141] transition">Contact</li>
       </ul>
 
- 
-      <div className="flex gap-[20px]">
-        <div className="p-[10px] pb-[6px] bg-[#fbfbfb] border border-[#ebebeb]">
-          <img src={instagram_icon} alt="" className="h-[30px]" />
-        </div>
-
-        <div className="p-[10px] pb-[6px] bg-[#fbfbfb] border border-[#ebebeb]">
-          <img src={facebook_icon} alt="" className="h-[30px]" />
-        </div>
-
-        <div className="p-[10px] pb-[6px] bg-[#fbfbfb] border border-[#ebebeb]">
-          <img src={whatsapp_icon} alt="" className="h-[30px]" />
-        </div>
+      <div className="flex gap-4 sm:gap-6">
+        {[instagram_icon, facebook_icon, whatsapp_icon].map((icon, idx) => (
+          <div
+            key={idx}
+            className="p-2 sm:p-3 bg-[#fbfbfb] border border-[#ebebeb] rounded cursor-pointer hover:bg-[#ff4141] hover:scale-110 transition"
+          >
+            <img src={icon} alt="social" className="h-6 sm:h-8" />
+          </div>
+        ))}
       </div>
 
-      
-      <div className="flex flex-col items-center gap-[30px] w-full mb-[30px] text-[#1a1a1a] text-[20px]">
-        <hr className="w-[80%] h-[3px] bg-[#c7c7c7] border-none rounded-[10px]" />
+      <div className="flex flex-col items-center gap-4 w-full text-[#1a1a1a] text-sm sm:text-base mt-6">
+        <hr className="w-11/12 h-[2px] bg-[#c7c7c7] border-none rounded" />
         <p>Copyright © 2023 - All Rights Reserved</p>
       </div>
     </div>
